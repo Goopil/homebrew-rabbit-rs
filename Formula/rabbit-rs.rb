a962a235ba4c1a7579ae096030089fa53ec9e6d4
Zachary Volpi <zachary.volpi@infomaniak.com>
2026-08-26 07:40:50 +0200

    Initial Homebrew formula for rabbit-rs

---
+++ Formula/rabbit-rs.rb
@@ -0,0 +1,71 @@
+class RabbitRs < Formula
+  desc "High-performance RabbitMQ transport for PHP, powered by Rust"
+  homepage "https://github.com/Goopil/rabbit-rs"
+  license "MIT"
+  version "0.0.6"
+
+  depends_on "php"
+
+  # macOS arm64 NTS only. Homebrew PHP is NTS-only on macOS.
+  resource "php84" do
+    url "https://github.com/Goopil/rabbit-rs/releases/download/v0.0.6/php_rabbit_rs-v0.0.6_php8.4-arm64-darwin-nts.zip"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  resource "php85" do
+    url "https://github.com/Goopil/rabbit-rs/releases/download/v0.0.6/php_rabbit_rs-v0.0.6_php8.5-arm64-darwin-nts.zip"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    php_version = Utils.safe_popen_read(Formula["php"].opt_bin/"php-config", "--version").strip
+    php_major_minor = php_version.split(".")[0, 2].join(".")
+
+    supported = ["8.4", "8.5"]
+    unless supported.include?(php_major_minor)
+      odie "rabbit-rs requires PHP 8.4 or 8.5. Found #{php_version}. Use PIE for other versions."
+    end
+
+    if Hardware::CPU.arch != :arm64
+      odie "rabbit-rs Homebrew formula supports Apple Silicon only. Use PIE on Intel Macs."
+    end
+
+    resource_name = "php" + php_major_minor.tr(".", "")
+    resource(resource_name).stage do
+      libexec.mkpath
+      cp "rabbit_rs.so", libexec/"rabbit_rs.so"
+    end
+  end
+
+  def post_install
+    ext_dir = Utils.safe_popen_read(Formula["php"].opt_bin/"php-config", "--extension-dir").strip
+
+    ini_path = etc/"php/conf.d/ext-rabbit_rs.ini"
+
+    ext_so = Pathname.new(ext_dir)/"rabbit_rs.so"
+    ohai "Installing rabbit_rs.so into #{ext_dir}"
+    ln_sf libexec/"rabbit_rs.so", ext_so
+
+    ohai "Creating INI file at #{ini_path}"
+    ini_path.dirname.mkpath
+    File.write(ini_path, "extension=rabbit_rs.so\n")
+  end
+
+  def uninstall
+    ini_path = etc/"php/conf.d/ext-rabbit_rs.ini"
+    ini_path.unlink if ini_path.exist?
+
+    ext_dir = Utils.safe_popen_read(Formula["php"].opt_bin/"php-config", "--extension-dir").strip
+    ext_so = Pathname.new(ext_dir)/"rabbit_rs.so"
+    ext_so.unlink if ext_so.symlink? && ext_so.exist?
+  end
+
+  test do
+    assert_match "rabbit_rs", shell_output("#{Formula["php"].opt_bin/"php"} -m")
+  end
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# homebrew-rabbit-rs
+
+Homebrew tap for [rabbit-rs](https://github.com/Goopil/rabbit-rs) -- a high-performance RabbitMQ transport for PHP and Laravel, powered by Rust.
+
+## Installation
+
+```bash
+brew tap goopil/rabbit-rs
+brew install rabbit-rs
+```
+
+## Requirements
+
+- macOS (Apple Silicon)
+- PHP 8.4 or 8.5 (installed via Homebrew)
+
+## Uninstall
+
+```bash
+brew uninstall rabbit-rs
+```
+
+## Alternative installation
+
+For Linux, Intel Macs, or other PHP versions, use [PIE](https://github.com/pie-php):
+
+```bash
+pie install rabbit-rs/native
+```
